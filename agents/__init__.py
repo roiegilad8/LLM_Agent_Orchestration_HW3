36a7bdd690ca144c6729b24363a82431064042f5
roiegilad8 <roiegilad@gmail.com>
2025-11-20 17:41:51 +0200

    Delete agents directory

--- agents/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Agent package for translation pipeline."""
-
-from .base import TranslationAgent
-from .dictionary_agent import DictionaryTranslationAgent
-
-__all__ = ["TranslationAgent", "DictionaryTranslationAgent"]
